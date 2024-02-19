b833969db1e0ad51d6d2288dfbc5f5ea0fff5732
Braxai <130407875+Braxai@users.noreply.github.com>
2024-02-18 19:43:53 -0800

    Create MenuBarSetup.java
    Added class to create Menu Bar

---
+++ src/MenuBarSetup.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MenuBarSetup {
+    public static JMenuBar setupMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        // File Menu
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem saveItem = new JMenuItem("Save");
+        saveItem.setMnemonic(KeyEvent.VK_S);
+        saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+        //saveItem.addActionListener(e -> saveImage());
+        fileMenu.add(saveItem);
+
+        JMenuItem loadItem = new JMenuItem("Load");
+        loadItem.setMnemonic(KeyEvent.VK_L);
+        loadItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_DOWN_MASK));
+        //loadItem.addActionListener(e -> loadImage());
+        fileMenu.add(loadItem);
+
+        fileMenu.add(new JSeparator()); // Separator
+
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener(e -> System.exit(0));
+        fileMenu.add(exitItem);
+
+        // Edit Menu
+        JMenu editMenu = new JMenu("Edit");
+
+        JMenuItem undoItem = new JMenuItem("Undo");
+        undoItem.setMnemonic(KeyEvent.VK_Z);
+        undoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
+        editMenu.add(undoItem);
+
+        JMenuItem redoItem = new JMenuItem("Redo");
+        redoItem.setMnemonic(KeyEvent.VK_X);
+        redoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+        editMenu.add(redoItem);
+
+        JMenuItem clearItem = new JMenuItem("Clear");
+        clearItem.setMnemonic(KeyEvent.VK_C);
+        clearItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+        editMenu.add(clearItem);
+
+        // Help Menu
+        JMenu helpMenu = new JMenu("Help");
+        JMenuItem aboutItem = new JMenuItem("About");
+        //aboutItem.addActionListener(e -> showAbout());
+        helpMenu.add(aboutItem);
+
+        // Canvas Menu
+        JMenu canvasMenu = new JMenu("Canvas");
+
+        JMenuItem resizeItem = new JMenuItem("Resize");
+        canvasMenu.add(resizeItem);
+
+        JMenuItem zoomInItem = new JMenuItem("Zoom In");
+        canvasMenu.add(zoomInItem);
+
+        JMenuItem zoomOutItem = new JMenuItem("Zoom Out");
+        canvasMenu.add(zoomOutItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(canvasMenu);
+        menuBar.add(helpMenu);
+
+        return menuBar;
+    }
+}
